5b797e3d0716d73fddc43fb015bee040258c2600
Mohammed Affan <affan2@gmail.com>
2019-08-19 17:07:10 +0400

    loop fix

--- smart_selects/views.py
+++ smart_selects/views.py
@@ -48,8 +48,8 @@ def do_filter(qs, keywords, exclude=False):
             values = value.split(",")
             if len(values) > 0:
                 or_q = Q()
-                for value in values:
-                    or_q |= Q(**{keyword: value})
+                for val in values:
+                    or_q |= Q(**{keyword: val})
                 and_q &= or_q
         except AttributeError:
             # value can be a bool
